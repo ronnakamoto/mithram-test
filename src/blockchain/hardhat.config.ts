82e78cbfbe609c376dd5506c40861ed73115856d
Ron Nakamoto <14256602+ronnakamoto@users.noreply.github.com>
2025-02-15 04:08:37 +0530

    feat: added support for sepolia

--- src/blockchain/hardhat.config.ts
+++ src/blockchain/hardhat.config.ts
@@ -19,6 +19,10 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.NFT_PRIVATE_KEY ? [process.env.NFT_PRIVATE_KEY] : [],
+    },
   },
   paths: {
     sources: "./contracts",
