41f75f8bad4c2c36c28a4949117560c134a33ed7
Ron Nakamoto <14256602+ronnakamoto@users.noreply.github.com>
2025-02-15 15:45:30 +0530

    chore: updated icon for chat

--- frontend/smart-app/src/components/AnalysisDetails.jsx
+++ frontend/smart-app/src/components/AnalysisDetails.jsx
@@ -13,6 +13,7 @@ import {
   SparklesIcon,
   LinkIcon,
   DocumentDuplicateIcon,
+  ChatBubbleLeftIcon
 } from '@heroicons/react/24/outline'
 
 const PatientCard = ({ patient }) => {
@@ -374,7 +375,7 @@ function AnalysisDetails({ id, setIsAuthenticated }) {
                   onClick={() => setShowChat(true)}
                   className="inline-flex items-center px-4 py-2 border border-transparent rounded-full text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 cursor-pointer space-x-2"
                 >
-                  <SparklesIcon className="h-4 w-4" />
+                  <ChatBubbleLeftIcon className="h-4 w-4" />
                   <span>Chat with Mithram</span>
                 </button>
               </div>
